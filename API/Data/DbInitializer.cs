cd2148cae3e1009c8ee635920150fdadeb24c6cb
Truman Hogg <hoggtruman@gmail.com>
2026-02-16 17:11:29 +0000

    Update Seeding
    Check if tables already contain data rather than emptying and repopulating every time

--- API/Data/DbInitializer.cs
+++ API/Data/DbInitializer.cs
@@ -25,48 +25,26 @@ public class DbInitializer : IDbInitializer
     /// </summary>
     public void SeedAll()
     {
-        ClearTables();
+        // ClearTables();
 
         // Ordered to prevent foreign key constraint violations
-        AddRecords<Gender, GenderCSVMap>(@"gender.csv", _dbContext.Gender);
-        AddRecords<Nature, NatureCSVMap>(@"nature.csv", _dbContext.Nature);
-        AddRecords<DamageClass, DamageClassCSVMap>(@"damage_class.csv", _dbContext.DamageClass);
-        AddRecords<PkmnType, PkmnTypeCSVMap>(@"pkmn_type.csv", _dbContext.PkmnType);
-        AddRecords<Ability, AbilityCSVMap>(@"ability.csv", _dbContext.Ability);
-        AddRecords<MoveEffect, MoveEffectCSVMap>(@"move_effect.csv", _dbContext.MoveEffect);
-        AddRecords<Item, ItemCSVMap>(@"item.csv", _dbContext.Item);
-        AddRecords<Move, MoveCSVMap>(@"move.csv", _dbContext.Move);
-        AddRecords<Pokemon, PokemonCSVMap>(@"pokemon.csv", _dbContext.Pokemon);
-        AddRecords<BaseStats, BaseStatsCSVMap>(@"base_stats.csv", _dbContext.BaseStats);
+        SeedTable<Gender, GenderCSVMap>(@"gender.csv", _dbContext.Gender);
+        SeedTable<Nature, NatureCSVMap>(@"nature.csv", _dbContext.Nature);
+        SeedTable<DamageClass, DamageClassCSVMap>(@"damage_class.csv", _dbContext.DamageClass);
+        SeedTable<PkmnType, PkmnTypeCSVMap>(@"pkmn_type.csv", _dbContext.PkmnType);
+        SeedTable<Ability, AbilityCSVMap>(@"ability.csv", _dbContext.Ability);
+        SeedTable<MoveEffect, MoveEffectCSVMap>(@"move_effect.csv", _dbContext.MoveEffect);
+        SeedTable<Item, ItemCSVMap>(@"item.csv", _dbContext.Item);
+        SeedTable<Move, MoveCSVMap>(@"move.csv", _dbContext.Move);
+        SeedTable<Pokemon, PokemonCSVMap>(@"pokemon.csv", _dbContext.Pokemon);
+        SeedTable<BaseStats, BaseStatsCSVMap>(@"base_stats.csv", _dbContext.BaseStats);
 
-        AddRecords<PokemonPkmnType, PokemonPkmnTypeCSVMap>(@"pokemon_pkmn_type.csv", _dbContext.PokemonPkmnType);
-        AddRecords<PokemonMove, PokemonMoveCSVMap>(@"pokemon_move.csv", _dbContext.PokemonMove);
-        AddRecords<PokemonAbility, PokemonAbilityCSVMap>(@"pokemon_ability.csv", _dbContext.PokemonAbility);
-        AddRecords<PokemonGender, PokemonGenderCSVMap>(@"pokemon_gender.csv", _dbContext.PokemonGender);
+        SeedTable<PokemonPkmnType, PokemonPkmnTypeCSVMap>(@"pokemon_pkmn_type.csv", _dbContext.PokemonPkmnType);
+        SeedTable<PokemonMove, PokemonMoveCSVMap>(@"pokemon_move.csv", _dbContext.PokemonMove);
+        SeedTable<PokemonAbility, PokemonAbilityCSVMap>(@"pokemon_ability.csv", _dbContext.PokemonAbility);
+        SeedTable<PokemonGender, PokemonGenderCSVMap>(@"pokemon_gender.csv", _dbContext.PokemonGender);
 
         _dbContext.BulkSaveChanges();
-    }
-
-
-    /// <summary>
-    /// Deletes all data from the seed data tables.
-    /// </summary>
-    private void ClearTables()
-    {
-        _dbContext.Pokemon.ExecuteDelete();
-        _dbContext.PkmnType.ExecuteDelete();
-        _dbContext.BaseStats.ExecuteDelete();
-        _dbContext.Ability.ExecuteDelete();
-        _dbContext.Move.ExecuteDelete();
-        _dbContext.DamageClass.ExecuteDelete();
-        _dbContext.MoveEffect.ExecuteDelete();
-        _dbContext.Gender.ExecuteDelete();
-        _dbContext.Item.ExecuteDelete();
-        _dbContext.Nature.ExecuteDelete();
-        _dbContext.PokemonPkmnType.ExecuteDelete();
-        _dbContext.PokemonMove.ExecuteDelete();
-        _dbContext.PokemonAbility.ExecuteDelete();
-        _dbContext.PokemonGender.ExecuteDelete();
     }
 
 
@@ -77,16 +55,19 @@ public class DbInitializer : IDbInitializer
     /// <typeparam name="M">The CSV class map type associated with T.</typeparam>
     /// <param name="filename">The filename containing the records.</param>
     /// <param name="dbSet">The DbSet to add entries to.</param>
-    private static void AddRecords<T, M>(string filename, DbSet<T> dbSet) 
+    private static void SeedTable<T, M>(string filename, DbSet<T> dbSet) 
         where M : ClassMap<T>
         where T : class
     {
-        using (var streamReader = new StreamReader(Path.Join(SeedDir, filename)))
-        using (var csvReader = new CsvReader(streamReader, CultureInfo.InvariantCulture))
+        if (dbSet.Any())
         {
-            csvReader.Context.RegisterClassMap<M>();
-            var records = csvReader.GetRecords<T>().ToArray();
-            dbSet.AddRange(records);
+            return;
         }
+
+        using var streamReader = new StreamReader(Path.Join(SeedDir, filename));
+        using var csvReader = new CsvReader(streamReader, CultureInfo.InvariantCulture);
+        csvReader.Context.RegisterClassMap<M>();
+        var records = csvReader.GetRecords<T>();
+        dbSet.AddRange(records);
     }
 }
